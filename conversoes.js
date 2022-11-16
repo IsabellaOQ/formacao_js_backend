6119c2790141889fcea7dd6bc5080776b403d469
IsabellaOQ <bella_oq@hotmail.com>
2022-11-16 20:30:44 -0300

    aula 2

--- conversoes.js
+++ conversoes.js
@@ -1,4 +1,4 @@
-// conversão implícita - conseguimos converter um tipo de dado em outro
+// conversão implícita
 const num = 456;
 const text = '456';
 
@@ -11,6 +11,13 @@ console.log(num + text); // realiza a concatenação
 
 // conversão explícita
 
-//String()
+// String()
 // Number()
 console.log(Number(text) + num); // realiza a conversão da variavel
+
+let telefone = 12341234;
+console.log('O telefone é ' + telefone.toString()); //.toString() é outra forma para fazer conversão
+
+let largura = '10';
+let altura = '5';
+console.log(+largura * +altura); //conversão de String para número usando o + antes das variáveis
