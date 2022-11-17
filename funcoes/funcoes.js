bb39b15f23a4f3983ffe2af82b449759103f4b80
IsabellaOQ <bella_oq@hotmail.com>
2022-11-16 21:32:20 -0300

    adicionando codigos js

---
+++ funcoes/funcoes.js
@@ -0,0 +1,14 @@
+// declara
+function imprimeTexto(texto) {
+	console.log(texto);
+}
+
+//executa
+imprimeTexto('oi!');
+imprimeTexto('hello!');
+
+function soma() {
+	return 2 + 2;
+}
+
+console.log(soma());
